8ef48cd3f5c8ec4f8a430a5de08880d3c255b9d2
prabhamanju <manjuparbha@gmail.com>
2022-12-29 06:37:32 +0530

    dictionaries in pythod

---
+++ Dictionary/dic_methods.py
@@ -0,0 +1,16 @@
+myDict ={
+    "firstName" : "Manju",
+    "lastName" : "Prab",
+    "marks": [82,83,92],
+    "hobby" : {"playing" : "reading" },
+    1 : 2
+    
+}
+
+print(myDict.keys())
+
+print(myDict.values())
+
+print(myDict.items())
+
+
