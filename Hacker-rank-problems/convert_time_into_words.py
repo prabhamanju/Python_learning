a4e0ff0ed550b1f3e4627a4f9585eafb94ddef56
prabhamanju <manjuparbha@gmail.com>
2023-01-10 23:19:48 +0530

    hackerrank_problem_convert_time_to words

---
+++ Hacker-rank-problems/convert_time_into_words.py
@@ -0,0 +1,58 @@
+def timeInWords(h, m):
+    n = [
+        "zero", 
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "quarter",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+        "twenty",
+        "twenty one",
+        "twenty two",
+        "twenty three",
+        "twenty four",
+        "twenty five",
+        "twenty six",
+        "twenty seven",
+        "twenty eight",
+        "twenty nine",
+        "half"
+    ]
+    m = int(m)
+    h = int(h)
+    o = ''
+    if not m:
+        return "%s o' clock" % n[h]
+    if m>30: 
+        m = 60 - m
+        w = "to"
+        h = (h+1)%12 or 12
+    else:
+        w = "past"
+        o = " minutes"
+    if m==1:
+        o = " minute"
+    elif not m%15:
+        o = ""
+    m = n[m]
+    h = n[h]
+    
+    return "%s%s %s %s"%(m,o,w,h)
+
+H = input("enter hour\n")
+M = input("enter minute\n")
+print(timeInWords(H, M))
