b9df93ed44524d6e81559cf40918bfa5467fe19c
prabhamanju <manjuparbha@gmail.com>
2023-01-23 04:35:22 +0530

    use of pow funtion

---
+++ Hacker-rank-problems/powuse.py
@@ -0,0 +1,13 @@
+"""Task
+Read four numbers,a,b,c, and d , and print the result of (a**b + c**d).
+"""
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+print(a,b,c,d)
+
+print(a,b,c,d)
+print(pow(a,b) + pow(c,d))
+print((a**b)+(c**d))
