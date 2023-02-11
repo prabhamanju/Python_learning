394ad556b2fe94433afd4d9042fa5f6b394fc265
prabhamanju <manjuparbha@gmail.com>
2023-02-12 04:01:15 +0530

    advance_concept_of_decorator

---
+++ Decorators/advance_concept_of_deco.py
@@ -0,0 +1,20 @@
+def smart_div(func):
+    print("execute" ,func.__name__, "function")
+    def inner(a,b):
+        if a < b:
+            a, b = b, a
+            func(a,b)
+        elif a > b:
+            func(a,b)
+
+    return inner
+
+@smart_div
+def find_div(a, b):
+    print(a//b)
+
+find_div(4,2)
+find_div(4,16)
+
+
+print(4//32)
